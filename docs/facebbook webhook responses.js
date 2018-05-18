f98aa2d342dea93197ab8f026cdf601d3200d5e8
logoskaizenphi <logoskaizenphi@gmail.com>
2018-05-17 18:51:49 -0700

    mmm,

---
+++ docs/facebbook webhook responses.js
@@ -0,0 +1,29 @@
+    //simple facebook text response example                            
+   //"text": "Hello, perseus you fucking epic stud you!"
+   //,
+    
+    //simpe facebook audio response example
+    // "attachment": {
+    //   "type": "audio",
+    //   "payload": {
+    //     "url": "https://ia802508.us.archive.org/5/items/testmp3testfile/mpthreetest.mp3"
+    //   }
+    // }
+
+
+    /*simple facebook quick reply text w/color example
+     "text": "What do you think?",
+         "quick_replies": [
+            {
+               "content_type": "text",
+               "title": "Short",
+               "payload": "red"
+            },
+            {
+               "content_type": "text",
+               "title": "Tall",
+               "payload": "green"
+            }
+         ]
+
+          *///end facebook text quick reply example
